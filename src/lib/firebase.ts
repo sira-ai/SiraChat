f157ac1bbb9794ee0724c79b5e59acc399aebbc6
BRAXX444 YOU <braxx444you@gmail.com>
2025-08-10 17:35:00 +0000

    lagi dan lagi error ini plis analisis ya aku cape

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,5 @@
-
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps, getApp } from "firebase/app";
+import { initializeApp, getApps, getApp, FirebaseApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
@@ -16,9 +15,14 @@ const firebaseConfig = {
   "messagingSenderId": "600267541023"
 };
 
-
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+let app: FirebaseApp;
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApp();
+}
+
 const db = getFirestore(app);
 const storage = getStorage(app);
 const auth = getAuth(app);
