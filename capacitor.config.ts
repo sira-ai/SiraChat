d13e9df03da93ec975b16de44d45fda252006aa7
sira-ai <slalu128@gmail.com>
2025-08-17 02:48:23 +0800

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'sira.chat',
   appName: 'SiraChat',
-  webDir: 'public'
+  webDir: 'out'
 };
 
 export default config;
